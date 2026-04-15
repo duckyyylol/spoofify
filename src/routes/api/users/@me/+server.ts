ffdb64e218f20419b4a205e97dbc9087f60a7812
ducky <asqrybot@gmail.com>
2026-04-15 00:35:26 -0400

    feat: allow api key to fetch user

--- src/routes/api/users/@me/+server.ts
+++ src/routes/api/users/@me/+server.ts
@@ -1,8 +1,17 @@
 import { Spotify } from '$lib';
+import { getSessionData } from '$lib/server/db/session.js';
 import { json } from '@sveltejs/kit';
 
-export const GET = async ({params, cookies, url}): Promise<Response> => {
+export const GET = async ({params, cookies, url, request}): Promise<Response> => {
     let tokenCookie = cookies.get("token-0") || null;
+    let apikey = request.headers.get("api-key") || null;
+
+    if(!tokenCookie && apikey) {
+        let sessionData = getSessionData();
+        if(sessionData) tokenCookie = sessionData.access_token;
+    }
+
+    console.log("TOKEN COOKIE", tokenCookie)
 
     let spotify = new Spotify(tokenCookie);
 
--- src/routes/auth/callback/+page.server.ts
+++ src/routes/auth/callback/+page.server.ts
@@ -1,4 +1,5 @@
 import { browser } from '$app/environment'
+import { deleteSessionData, getSessionData } from '$lib/server/db/session.js'
 import { redirect } from '@sveltejs/kit'
 import { setUserData, UserRoles } from 'duckylib'
 
@@ -11,7 +12,9 @@ export const load = async ({fetch, cookies, url}) => {
     console.log("CALLBACK USER", currentUser)
 
     if(!currentUser) {
-        if(!url.searchParams.has("out")) return redirect(308, "/auth")
+        if(!url.searchParams.has("out")) return redirect(308, "/auth");
+        let sd = getSessionData();
+        if(sd?.access_token) await deleteSessionData(sd.access_token);
         return {user: null};
     } else {
         return {user: {avatarUrl: currentUser.images[0].url, id: currentUser.id, role: UserRoles.ADMIN, username: currentUser.display_name}}
